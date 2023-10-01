a04e5c2d47c30caddb0c68b6e45c93d9bd00767a
snorkungen <teuvo2004@gamil.com>
2023-10-01 10:48:46 +0300

    second commit

--- src/main.ts
+++ src/main.ts
@@ -2,47 +2,97 @@ import { createElement } from './lib/create-element';
 import './style.css'
 
 
-const textBox = document.getElementById("textbox")
+const textBox = document.getElementById("textbox") as HTMLElement
 const colWidth = 80;
 
-const text = `IGPAY ATINLAY SIAY NAAY NGLISHEAY ANGUAGELAY ORDWAY
- AME GAY ROAY RGOT.AAY NGLISHEAY ORDSWAY REAAYLTEREDAAY YBAY DJUSTINGAAY OSITIONSPAY FOAY NEOAY ROAY OREMAY ETTERSLAY NDAAY DDINGAAY DDITIONALAAY UFFIXES.SAY HETAY OALGAY SIAY OTAY NDEAY PUAY ITHWAY AAY ORDWAY IMILARSAY NOUGHEAY OTAY HETAY RIGINALOAY OTAY EBAY NDERSTOODUAY YBAY OMEONESAY HOWAY SIAY AMILIARFAY ITHWAY HETAY ANGUAGELAY UTBAY LSOAAY IFFERENTDAY NOUGHEAY OTNAY OTAY EBAY NDERSTOODUAY YBAY OMEONESAY LSE.EAY`
+const text = `#include <stdio.h>
+int main() {
+\tint t1 = 0, t2 = 1, nextTerm = 0, n;
+\tprintf("Enter a positive number: ");
+\tscanf("%d", &n);
+
+\t// displays the first two terms which is always 0 and 1
+\tprintf("Fibonacci Series: %d, %d, ", t1, t2);
+\tnextTerm = t1 + t2;
+
+\twhile (nextTerm <= n) {
+\t\tprintf("%d, ", nextTerm);
+\t\tt1 = t2;
+\t\tt2 = nextTerm;
+\t\tnextTerm = t1 + t2;
+\t}
+
+\treturn 0;
+}
+`
+
 
 if (!(textBox instanceof HTMLElement)) {
   throw 0
 }
 
-const matrix: string[][] = []
+const NEW_LINE = "Enter"
+const TAB = "Tab"
+const BACKSPACE = "Backspace"
 
-let rowIdx = 0;
-for (let i = 0; i < text.length; i++) {
-  let c = text[i]
+function processText(text: string) {
+  let matrix: string[][] = [];
+  let rowIdx = 0;
 
-  // if prev is new line do something
+  for (let i = 0; i < text.length; i++) {
+    let char = text[i]
 
-  if (text[i-1] == "\n") {
-    rowIdx += 1;
+    switch (char) {
+      case "\n": char = NEW_LINE; break
+      case "\t": char = TAB; break
+    }
+
+    if (matrix[rowIdx] == undefined) {
+      matrix[rowIdx] = [];
+    }
+
+    matrix[rowIdx].push(char)
+
+
+    if (
+      char == NEW_LINE
+      || matrix[rowIdx].length >= colWidth
+    ) {
+      rowIdx++
+    }
+
   }
 
-
-  if (matrix[rowIdx] == undefined) {
-    matrix[rowIdx] = [];
-  }
-
-
-  matrix[rowIdx].push(c)
-
-  if (matrix[rowIdx].length >= colWidth) {
-    rowIdx++
-  }
+  return matrix
 }
 
+function fillTextBox(textBox: HTMLElement, matrix: string[][]): boolean {
 
-// U+02936
+  let displayChar = (char: string) => {
 
-for (let row of matrix) {
-  createElement(textBox, "div", "class=row", { children: row.map(c => createElement(null, "span", "class=row-item", `content=${c}`)) })
+    switch (char) {
+      case NEW_LINE: return "&#10550;"
+      case TAB: return "&nbsp;&lrarr;&nbsp;"
+    }
+    return char;
+  }
+
+  for (let row of matrix) {
+    createElement(textBox, "div", "class=row", {
+      children: row.map(char => createElement(null, "span", "class=row-item", {
+        children: displayChar(char),
+        attributes: {
+          "data-char": char
+        }
+
+      }))
+    })
+  }
+
+  return true;
 }
+
+fillTextBox(textBox!, processText(text))
 
 const state = {
   row: 0,
@@ -51,53 +101,75 @@ const state = {
   done: false
 }
 
-
-function resolveKey(str: string) {
-
-  switch (str) {
-
-    case "Space": return " "
-    case "Enter": return "\n"
-    case "Tab": return "\t"
-  }
-
-
-  return str;
-}
-
-
-function setActive() {
+function getActiveChar(): HTMLElement | undefined {
   if (state.done) {
     return;
   }
 
   let row = textBox!.children[state.row];
-  let element = row.children[state.index]
+  let element = row.children[state.index];
+
+  return element as HTMLElement;
+}
+
+function setActive() {
+  let element = getActiveChar()
+
+  if (!element) return;
 
   element.classList.add("item-active")
 
 }
 
-window.addEventListener("keypress", function (event) {
-
+window.addEventListener("keydown", function (event) {
+  event.stopImmediatePropagation()
+  event.preventDefault()
   if (state.done) {
     console.log("Done")
     return false;
   }
 
-  const key = resolveKey(event.key);
+  const key = event.key
 
-  if (key == "Backspace") {
 
+  console.log(key)
+  // ignore if special char
+  if (key.length > 1 && ![NEW_LINE, TAB, BACKSPACE].includes(key)) {
+    return;
   }
 
+  getActiveChar()!.classList.remove("item-active")
+
   let row = textBox.children[state.row];
-  let element = row.children[state.index]
+
+  if (key == BACKSPACE) {
+    // move state back
+
+    if (state.row == 0 && state.index == 0) return;
+
+    if (state.index > 0) {
+      state.index--;
+    } else {
+      state.row--;
+      state.index = textBox.children[state.row].children.length - 1
+    }
+
+    let el = getActiveChar()!
+
+    el.classList.remove("item-1" ,"item-2")
+
+    setActive()
+
+    return;
+  }
+
+
+  let element = row.children[state.index] as HTMLElement;
 
 
 
   element.classList.remove("item-active")
-  if (element.textContent == key) {
+  if (element.dataset.char == key) {
     element.classList.add("item-1")
   } else {
     element.classList.add("item-2")
